fedd95c89482011615b7469a7568e1fa5925f3de
Amir-H-Shzd <amirshir737@gmail.com>
2021-09-07 12:23:15 +0430

    create basic not standard page
    i created two paths names test return http test and json return json method
    and created views file in mysite file to run those.
    
    in next commit I'm going to create those with standard method with creating an app

---
+++ mysite/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse, JsonResponse
+
+
+def test(request):
+    return HttpResponse('<h1>Hi Amir Hossein</h1>')
+
+
+def json(request):
+    return JsonResponse({'name': 'Amir Hossein'})
