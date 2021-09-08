3f927ffb4f2a88ea03c5801ab67b896d15d720d2
Amir-H-Shzd <amirshir737@gmail.com>
2021-09-08 15:56:07 +0430

    using templates folder to create html pages
    create home , contact  , about.html in templates folder and use render to run those pages in views.py

--- website/views.py
+++ website/views.py
@@ -2,13 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+# def Home_view(request):
+#     return HttpResponse('<h1>Home</h1>')
+
+
+# def Home_view(request):
+#     return render(request, 'home.html')
+
+
 def Home_view(request):
-    return HttpResponse('<h1>Home</h1>')
+    return render(request, 'website_folder/home.html')
+    # Note: if you added a folder in templates folder
+    # you need to change the address from 'home.html' to 'foldername/home.html'
 
 
 def Contact_view(request):
-    return HttpResponse('<h1>Contact</h1>')
+    return render(request, 'website_folder/contact.html')
 
 
 def About_us_view(request):
-    return HttpResponse('<h1>About us</h1>')
+    return render(request, 'website_folder/about.html')
