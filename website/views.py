c176f1c68ba6dc32f7d445186a1c2d755d1ed12d
Amir-H-Shzd <amirshir737@gmail.com>
2021-09-09 14:40:35 +0430

    adding general templates folder
    created this general folder to gather all apps templates in one folder.
    then changed 'DIRS' in settings.py to '[BASE_DIR / 'templates']' to run pages in new path

--- website/views.py
+++ website/views.py
@@ -11,14 +11,14 @@ from django.http import HttpResponse
 
 
 def Home_view(request):
-    return render(request, 'website_folder/home.html')
+    return render(request, 'website/home.html')
     # Note: if you added a folder in templates folder
     # you need to change the address from 'home.html' to 'foldername/home.html'
 
 
 def Contact_view(request):
-    return render(request, 'website_folder/contact.html')
+    return render(request, 'website/contact.html')
 
 
 def About_us_view(request):
-    return render(request, 'website_folder/about.html')
+    return render(request, 'website/about.html')
