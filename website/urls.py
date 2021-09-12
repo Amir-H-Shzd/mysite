0d7c5c61c84121e64e3e5eb2c72e4b270d3ba985
Amir-H-Shzd <amirshir737@gmail.com>
2021-09-12 16:05:18 +0430

    linking pages with url template tag
    -added app_name = 'website' to urls.py
    -changing file like (home , contact , about) names to url tags. so when we click on those name in home page we go to those pages

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from website.views import Elements_view, Home_view, Contact_view, About_us_view
 
+app_name = 'website'
+
 urlpatterns = [
-    path('', Home_view),
-    path('contact/', Contact_view),
-    path('about/', About_us_view),
-    path('element/', Elements_view)
+    path('', Home_view, name='home'),
+    path('contact/', Contact_view, name='contact'),
+    path('about/', About_us_view, name='about'),
+    path('element/', Elements_view, name='element')
 ]
