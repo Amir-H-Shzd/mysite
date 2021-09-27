cce5994e27a6ba6e7c5ca12526b730f1612c86c5
Amir-H-Shzd <amirshir737@gmail.com>
2021-09-27 23:54:20 +0330

    Update admin.py
    add some feature to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from blog.models import Post
 # Register your models here.
 
-admin.site.register(Post)
+# admin.site.register(Post)  || another method for register post to admin panel
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('title', 'created_date',
+                    'published_date', 'views', 'status')
+    list_filter = ('status',)
+    ordering = ['created_date']
+    search_fields = ['title', 'content']
