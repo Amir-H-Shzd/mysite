572f278585a3dda2e9a5f822603b1186d0642b95
Amir-H-Shzd <amirshir737@gmail.com>
2021-09-28 18:52:17 +0330

    some edit on class Contact
    -some changes in django panel admin showing
    -register class Contact

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from website.models import Contact
 
 # Register your models here.
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('name', 'email',
+                    'created_date')
+    list_filter = ('subject', 'email')
+    # ordering = ['created_date']
+    search_fields = ['name', 'message']
