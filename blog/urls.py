162bb0931ab73c58826006a1e08b152a47547f30
Amir-H-Shzd <amirshir737@gmail.com>
2021-09-29 22:30:55 +0330

    url parameters
    -create 404 page for pages that do not exist
    -use post id in urls to show them in page

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', blog_view, name='blog'),
     path('single/', single_view, name='single'),
-    path('test/', test, name='test'),
+    path('post-<int:pid>', test, name='test'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 
@@ -12,7 +12,8 @@ def single_view(request):
     return render(request, 'blog/blog-single.html')
 
 
-def test(request):
-    posts = Post.objects.all()
-    context = {'posts': posts}
+def test(request, pid):
+    #post = Post.objects.get(id=pid)
+    post = get_object_or_404(Post, id=pid)
+    context = {'post': post}
     return render(request, 'test.html', context)
