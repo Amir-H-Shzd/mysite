052b00e387ec85741d720c9d083030a996f6c66e
Amir-H-Shzd <amirshir737@gmail.com>
2021-10-04 21:34:21 +0330

    Update views.py
    edit some comment

--- blog/views.py
+++ blog/views.py
@@ -6,19 +6,15 @@ from django.utils import timezone
 def blog_view(request):
     # posts = Post.objects.filter(status=1)
     posts = Post.objects.filter(published_date__lte=timezone.now())
-    # posts = Post.objects.get(id=pid)
-    # posts = get_object_or_404(Post, id=pid)
-    # posts.views = posts.views + 1
-    # posts.save()
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
 
 def single_view(request, pid):
-    post = get_object_or_404(Post, id=pid)
     # post = Post.objects.get(id=pid)
-    # posts.views = posts.views + 1
-    # posts.save()
+    post = get_object_or_404(Post, id=pid)
+    post.views = post.views + 1
+    post.save()
     context = {'post': post}
     return render(request, 'blog/blog-single.html', context)
 
