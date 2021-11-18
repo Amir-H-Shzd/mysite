512a4106fb455853bff209b45aa615854014603f
Amir-H-Shzd <amirshir737@gmail.com>
2021-11-18 16:11:12 +0330

    custom template tag|filter
    when we want to apply a template filter to an query use | after that item

--- blog/templatetags/blog-tags.py
+++ blog/templatetags/blog-tags.py
@@ -7,3 +7,13 @@ register = template.Library()
 def function():
     posts = Post.objects.filter()
     return posts
+
+
+@register.simple_tag(name='plus')
+def numfunction(a):
+    return a + 2
+
+
+@register.filter
+def snippet(value):
+    return value[:100]+'...'
